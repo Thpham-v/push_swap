c99ed84f59e80edd75ed63a1096805e839d9f0ac
Thibault PHAM-VAN <thpham-v@e1r12p3.42.fr>
2021-11-02 18:00:11 +0100

    just_sort_five

--- main.c
+++ main.c
@@ -6,11 +6,58 @@
 /*   By: thpham-v <thpham-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 18:48:14 by thpham-v          #+#    #+#             */
-/*   Updated: 2021/10/31 16:53:10 by thpham-v         ###   ########.fr       */
+/*   Updated: 2021/11/02 17:58:06 by thpham-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sort_three(t_tab *tabs, t_var *var)
+{
+	if (tabs->tab1[0] > tabs->tab1[1] && tabs->tab1[0]
+		< tabs->tab1[2] && tabs->tab1[1] < tabs->tab1[2])
+		swap_a(tabs, var);
+	if (tabs->tab1[0] > tabs->tab1[1] && tabs->tab1[0]
+		> tabs->tab1[2] && tabs->tab1[1] > tabs->tab1[2])
+	{
+		swap_a(tabs, var);
+		reverse_r_a(tabs, var);
+	}
+	if (tabs->tab1[0] > tabs->tab1[1] && tabs->tab1[0]
+		> tabs->tab1[2] && tabs->tab1[1] < tabs->tab1[2])
+		rotate_a(tabs, var);
+	if (tabs->tab1[0] < tabs->tab1[1] && tabs->tab1[0]
+		< tabs->tab1[2] && tabs->tab1[1] > tabs->tab1[2])
+	{
+		swap_a(tabs, var);
+		rotate_a(tabs, var);
+	}
+	if (tabs->tab1[0] < tabs->tab1[1] && tabs->tab1[0]
+		> tabs->tab1[2] && tabs->tab1[1] > tabs->tab1[2])
+		reverse_r_a(tabs, var);
+}
+
+void	sort_five(t_tab *tabs, t_var *var)
+{
+	int	i;
+	int j;
+	int	count;
+	
+	i = 0;
+	count = 0;
+	push_b(tabs, var);
+	push_b(tabs, var);
+	sort_three(tabs, var);
+	
+}
+
+void	sort_small(t_tab *tabs, t_var *var, int argc)
+{
+	if (argc == 4)
+		sort_three(tabs, var);
+	if (argc == 6)
+		sort_five(tabs, var);
+}
 
 int	best_chunk(t_tab *tabs, t_var *var, int argc, char **argv)
 {
@@ -54,13 +101,23 @@ int	main(int argc, char **argv)
 	if (ret == -1)
 	{
 		free_tab(&tabs);
-		return (1);
+		return (-1);
 	}
 	var.chunk_size = ret;
 	if (init_tab(argc, argv, &tabs) == -1)
 		return (-1);
 	var.display++;
-	algo(&tabs, &var);
+	if (argc == 4 || argc == 6)
+		sort_small(&tabs, &var, argc);
+	else
+		algo(&tabs, &var);
+	i = 0;
+	while (i < tabs.index_a)
+	{
+		printf("tab a = %ld\n", tabs.tab1[i]);
+		i++;
+	}
+	printf("\n");
 	free_tab(&tabs);
 	return (0);
 }
